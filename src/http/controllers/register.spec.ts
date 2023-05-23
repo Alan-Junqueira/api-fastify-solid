2d5752ab27a26cbf9d23e5f7d064abcfead22276
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-05-23 17:00:06 -0300

    feat: create register controller test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,25 @@
-import { test } from "vitest";
+import request from "supertest"
+import { app } from "@/app"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
 
-test('ok', () => {
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+  
+  afterAll(async () => {
+    app.close()
+  })
 
+  it('Should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: "Jhon Doe",
+        email: "jhondoe@example.com",
+        password: "123456"
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
